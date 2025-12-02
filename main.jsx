6fbf192a673f9fd266c5cb78435df20e60ee6be1
Matthew Musson <matthewmmusson@gmail.com>
2025-12-01 22:12:05 -0800

    Viz v2

---
+++ main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import BottleRocketSim from './BottleRocketSim'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <BottleRocketSim />
+    </React.StrictMode>,
+)
